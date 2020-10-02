3b190dc53b5ba19379ff1b38b2853ec2faacff83
Alrin321 <56600823+Alrin321@users.noreply.github.com>
2020-10-02 20:17:59 +0530

    Update and rename #TEMPLATE.c to Alrin321.c

---
+++ 4.C/Alrin321.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Alrin George Sabu\n");
+     printf("Age    : 19\n");
+     printf("Username : Alrin321\n");
+     return(0);
+  }
