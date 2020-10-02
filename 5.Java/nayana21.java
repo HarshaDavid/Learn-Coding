b3885f854039afd5c86dbbb539a62d47813276c4
nayana girish <62895110+nayana21@users.noreply.github.com>
2020-10-02 21:58:51 +0530

    Create nayana21.java

---
+++ 5.Java/nayana21.java
@@ -0,0 +1,10 @@
+class nayana21{
+    public static void main(String[] args) {
+
+    String name = "Nayana Girish ";
+    int age = 21;
+    String username = "nayana21";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
