fcdbbeb80b35ac5cd10c7f76abd8f12a37e322b3
cyberlama <60261069+Aakash-cyberlama@users.noreply.github.com>
2020-10-02 20:25:41 +0530

    Create Aakash-Cyberlama.c

---
+++ 4.C/Aakash-Cyberlama.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Aakash k s\n");
+     printf("Age    : 19\n");
+     printf("Username : Aakash-Cyberlama\n");
+     return(0);
+  }
