84387f4972c21a96c8cc94462df1ade86fa0629d
Avani2000 <72266883+Avani2000@users.noreply.github.com>
2020-10-02 20:52:30 +0530

    Create Avani2000.java

---
+++ 5.Java/Avani2000.java
@@ -0,0 +1,10 @@
+class Avani2000 {
+    public static void main(String[] args) {
+
+    String name = "Avani";
+    int age = 20 ;
+    String username = "Avani2000";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
