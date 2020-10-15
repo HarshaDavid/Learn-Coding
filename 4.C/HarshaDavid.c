41a0dbb27bc2ce94b93dfebe0e3b63b3c0e24e6d
HarshaDavid <72264522+HarshaDavid@users.noreply.github.com>
2020-10-15 20:14:06 +0530

    Create HarshaDavid.c

---
+++ 4.C/HarshaDavid.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Harsha David\n");
+     printf("Age    : 20\n");
+     printf("Username : HarshaDavid\n");
+     return(0);
+  }
