3caba4fc69c0ad24b7909602c710120ed49d3451
HarshaDavid <72264522+HarshaDavid@users.noreply.github.com>
2020-10-15 20:16:39 +0530

    Create HarshaDavid.java

---
+++ 5.Java/HarshaDavid.java
@@ -0,0 +1,10 @@
+class HarshaDavid {
+    public static void main(String[] args) {
+
+    String name = "Harsha David";
+    int age = 20 ;
+    String username = "HarshaDavid";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
